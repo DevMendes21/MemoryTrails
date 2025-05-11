efb37e61639e2345d7f9881dbd10c685192a8e34
Yago Mendes <ygmendes21@gmail.com>
2025-05-11 20:10:29 -0300

    Update FormJogo.Designer.cs

--- TrilhasDaMemoria/FormJogo.Designer.cs
+++ TrilhasDaMemoria/FormJogo.Designer.cs
@@ -51,7 +51,7 @@ namespace TrilhasDaMemoria
             this.pnlInfo.Controls.Add(this.btnVoltar);
             // Adicionar botão Sair
             this.btnSair = new System.Windows.Forms.Button();
-            this.btnSair.Location = new System.Drawing.Point(300, 55); // Abaixo de Tempo
+            this.btnSair.Location = new System.Drawing.Point(220, 55); // Mais pru00f3ximo dos outros botu00f5es
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(100, 35);
             this.btnSair.TabIndex = 7;
@@ -115,7 +115,7 @@ namespace TrilhasDaMemoria
             this.btnVoltar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnVoltar.BackColor = System.Drawing.Color.LightCoral;
             this.btnVoltar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnVoltar.Location = new System.Drawing.Point(160, 55); // Abaixo de Pares
+            this.btnVoltar.Location = new System.Drawing.Point(120, 55); // Mais pru00f3ximo do Reiniciar
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(100, 35);
             this.btnVoltar.TabIndex = 0;
